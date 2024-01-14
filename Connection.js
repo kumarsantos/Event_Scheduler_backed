c7b23aa6525d961b83282333624c36a99f8488a1
Santosh <santosh.sah@mpoweredhealth.com>
2024-01-14 17:51:53 +0530

    catch removed during connection db

--- Connection.js
+++ Connection.js
@@ -5,7 +5,7 @@ const connectDB = () => {
     mongoose.connect(process.env.MONGODBURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err) => {
         if (err) throw err;
         console.log("connected to mongodb...!");
-    }).then(error => console.log(error));
+    });
 }
 
 module.exports = connectDB;
